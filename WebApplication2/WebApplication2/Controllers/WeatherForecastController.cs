0f73aa00a6f5717534f870ae512505f8b0baf58f
keshav.k <keshav.k@ME-LT6-DEV>
2020-09-10 17:56:33 +0530

    2nd commit

--- WebApplication2/WebApplication2/Controllers/WeatherForecastController.cs
+++ WebApplication2/WebApplication2/Controllers/WeatherForecastController.cs
@@ -21,6 +21,7 @@ namespace WebApplication2.Controllers
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
+
         }
 
         [HttpGet]
